aaf3f6a3d7ff76d7e211f888873813e40a8d14cf
briciovellaz <94410407+briciovellaz@users.noreply.github.com>
2023-01-11 10:58:02 -0300

    Add files via upload

---
+++ Grafo.h
@@ -0,0 +1,273 @@
+#ifndef GRAFO_H_
+#define GRAFO_H_
+
+#include <list>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template <typename C> class Grafo  // Grafo no dirigido.
+{
+
+public:
+    typedef pair<int, C> Arco;
+    typedef pair<int, list<Arco>> Nodo;
+	// NOTA: Dependiendo de la implementación puede ser necesario incluir otras funciones constructuras
+	Grafo();
+
+	~Grafo();
+    // Indica si el grafo tiene vertices o no. Complejidad O(1).
+	bool esta_vacio() const;
+
+    // Devuelve la cantidad de vertices adyacentes que tiene un vertice. Complejidad O(n)
+	int cantidad_adyacentes(int vertice);
+
+    //Devuelve la cantidad de vertices del grafo. Complejidad O(1).
+	int devolver_longitud() const;
+
+	//Indica si existe un vertice. Complejidad O(n).
+	bool existe_vertice(int vertice) const;
+
+    //Indica si existe un arco entre dos vertices. Complejidad O(n)
+	bool existe_arco(int origen, int destino) const;
+
+	// Devuelve el costo de un arco (origen,destino). Complejidad O(n).
+	const C & costo_arco(int origen, int destino) const;
+
+    // Recibe una lista vacia y la devuelve con los vertices del grafo en el orden que fueron agregados. Complejidad O(n).
+	void devolver_vertices(list<int> & vertices) const;
+
+    // Recibe una lista vacia y la devuelve con todos los adyacentes de un vertice. Complejidad O(n)
+	void devolver_adyacentes(int origen, list<Arco> & adyacentes) const;
+
+    //Agrega un vertice al grafo. Complejidad O(1)
+	void agregar_vertice(int vertice);
+
+    //Elimina un vertice y todos sus arcos. Complejidad O(n^2) en el peor caso (vertice conectado a todos los demas Nodos).
+	void eliminar_vertice(int vertice);
+
+    //Modifica el costo de un arco entre 2 vertices. Complejidad O(n)
+	void modificar_costo_arco(int origen, int destino, const C & costo);
+
+    // Conecta 2 vertices. Complejidad O(n)
+	void agregar_arco(int origen, int destino, const C & costo);
+
+    // Desconecta 2 vertices. Complejidad O(n)
+	void eliminar_arco(int origen, int destino);
+
+    // Vacia el grafo. Complejidad O(1).
+	void vaciar();
+
+private:
+
+    list<Nodo> grafo;
+
+};
+
+template <typename C> Grafo<C>::Grafo(){}
+template <typename C>	Grafo<C>::~Grafo(){}
+
+
+
+template <typename C> bool Grafo<C>::esta_vacio() const
+{
+    return grafo.empty();
+}
+
+template<typename C> int Grafo<C>::cantidad_adyacentes(int vertice){
+    if(existe_vertice(vertice)){
+        auto it=grafo.begin();
+        while(it->first!=vertice){
+            it++;
+        }
+        return(it->second.size()-1);
+    }
+    return -1;
+}
+
+template <typename C> int Grafo<C>::devolver_longitud() const {
+    return grafo.size();
+}
+
+template <typename C> bool Grafo<C>::existe_vertice(int buscado) const{
+    for(auto it=grafo.begin();it!=grafo.end();it++){
+        if (it->first==buscado){
+            return true;
+        }
+    }
+    return false;
+
+}
+
+template <typename C> bool Grafo<C>::existe_arco(int origen, int destino) const{
+    if (existe_vertice(origen)&&existe_vertice(destino)){
+        auto it= grafo.begin();
+        while (it->first!=origen){
+            it++;
+        }
+        if (it->first==origen){
+                auto itady= it->second.begin();
+                while (itady != it->second.end() && itady->first!=destino){
+                    itady++;
+                }
+                if (itady->first== destino){
+                    return true;
+                }
+
+        }
+    }
+    return false;
+
+}
+
+template <typename C> const C & Grafo<C>::costo_arco(int origen, int destino) const{
+    if (existe_arco(origen,destino)){
+        auto it= grafo.begin();
+        while (it->first!=origen){
+            it++;
+        }
+        auto itady= it->second.begin();
+        while (itady != it->second.end() && itady->first!=destino){
+            itady++;
+        }
+
+        return itady->second;
+    }
+}
+
+template <typename C> void Grafo<C>::devolver_vertices(list<int> & verts)const{
+    auto it = grafo.begin();
+    while(it!=grafo.end()){
+        verts.push_back(it->first);
+        it++;
+    }
+}
+
+template <typename C> void Grafo<C>::devolver_adyacentes(int origen, list<Arco> & adyacentes)const{
+   if (existe_vertice(origen)){
+        auto it= grafo.begin();
+        while (it->first!=origen){
+            it++;
+        }
+            for(auto ady =it->second.begin();ady!=it->second.end();ady++){
+                if(origen!=ady->first){
+                   adyacentes.push_back(*ady);
+                }
+            }
+    }
+}
+
+template <typename C> void Grafo<C>::agregar_vertice(int nuevo){
+    if(!existe_vertice(nuevo)){
+        if (nuevo<0){nuevo=-nuevo;} //no se permiten vertices con indice negativo
+        Nodo vertnuevo;
+        vertnuevo.first=nuevo;
+        vertnuevo.second.push_back(make_pair(nuevo,0));
+        grafo.push_back(vertnuevo);
+    }
+}
+
+template <typename C> void Grafo<C>::eliminar_vertice(int vert){
+
+    if (existe_vertice(vert)){
+
+        list<pair<int,int>> adyacentes;
+        devolver_adyacentes(vert,adyacentes);
+        ;
+        for(auto itAdy=adyacentes.begin();itAdy!=adyacentes.end();itAdy++){
+            eliminar_arco(vert,itAdy->first);
+        }
+        auto it=grafo.begin();
+        while (it!=grafo.end()&& it->first!= vert){
+            it++;
+        }
+        it->second.clear();
+        it= grafo.erase(it);
+    }
+}
+
+template <typename C> void Grafo<C>:: modificar_costo_arco(int origen, int destino, const C & costo){
+    if (existe_arco(origen,destino)){
+        auto it=grafo.begin();
+        while (it->first!= origen){it++;}
+        auto itady=it->second.begin();
+        while (itady->first!=destino) {itady++;}
+        itady->second=costo;
+        it=grafo.begin();
+        while (it->first!= destino){it++;}
+        itady=it->second.begin();
+        while (itady->first!=origen) {itady++;}
+        itady->second=costo;
+    }
+}
+
+template<typename C> void Grafo<C>:: agregar_arco(int origen, int destino, const C & costo){
+   if (existe_vertice(destino)&& existe_vertice(origen)){
+        Arco nuevo;
+        nuevo.first=destino;
+        nuevo.second=costo;
+        auto it=grafo.begin();
+        while(it->first!=origen){it++;}
+        it->second.push_back(nuevo);
+        nuevo.first=origen;
+        it=grafo.begin();
+        while(it->first!=destino){it++;}
+        it->second.push_back(nuevo);
+
+    }
+}
+
+template <typename C> void Grafo<C>::eliminar_arco(int origen, int destino){
+
+    if (existe_arco(origen,destino)){
+        pair<int, C> borrar;
+        borrar.first=destino;
+        borrar.second=costo_arco(origen,destino);
+
+        auto it= grafo.begin();
+        while (it->first!=origen && it!= grafo.end() ){
+            it++;
+        }
+        it->second.remove(borrar);
+        borrar.first=origen;
+        it =grafo.begin();
+        while (it->first!=destino && it!= grafo.end() ){
+            it++;
+        }
+        it->second.remove(borrar);
+    }
+}
+
+template <typename C> void Grafo<C>::vaciar(){
+    grafo.clear();
+}
+
+// Sobrecarga el operador << para que el grafo pueda ser mostrado por consola. Complejidad O(n^2) en el peor caso(todos los Nodos conectados).
+template <typename C> ostream & operator << (ostream & salida, const Grafo<C> & grafo)
+{
+    if(!grafo.esta_vacio()){
+        // Recorremos todos los vertices
+        list<int> vertices;
+        grafo.devolver_vertices(vertices);
+        list<int>::iterator v = vertices.begin();
+        while (v != vertices.end()) {
+            salida << "\n    " << *v << ":\n";
+            // Recorremos todos los adyacentes de cada vertice
+            list<pair<int,C>> adyacentes;
+            grafo.devolver_adyacentes(*v, adyacentes);
+            typename list<typename Grafo<C>::Arco>::iterator ady = adyacentes.begin();
+            while (ady != adyacentes.end()) {
+                if(ady->first!=*v){salida << "    " << *v << "-> " << ady->first << " (" << ady->second << ")\n";}
+                ady++;
+            }
+            v++;
+        }
+        return salida;
+    }
+    salida << " El grafo no tiene vertices.\n";
+    return salida;
+
+}
+
+#endif /* GRAFO_H_ */
---
+++ main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <list>
+#include "Grafo.h"
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+void CoberturaBomberos(Grafo<int> grafo);
+void ActualizarAdyacentes(vector<pair<int,int>> &barrios,Grafo<int> grafo,list<int> vertices);
+bool OrdenPorSeg(const pair<int,int> &a,const pair<int,int> &b);
+void OrdenarDec(vector<pair<int,int>> &lista);
+
+int main()
+{
+	Grafo<int> grafo;
+
+	for(int i=1;i<13;i++){          //Ejemplo
+        grafo.agregar_vertice(i);
+	}
+
+	grafo.agregar_arco(1,2,0);  //Ciudad ejemplo TPE3 A
+	grafo.agregar_arco(1,3,0);
+	grafo.agregar_arco(1,5,0);
+	grafo.agregar_arco(2,5,0);
+	grafo.agregar_arco(3,5,0);
+	grafo.agregar_arco(3,4,0);
+	grafo.agregar_arco(3,6,0);
+	grafo.agregar_arco(3,7,0);
+	grafo.agregar_arco(3,8,0);
+	grafo.agregar_arco(4,5,0);
+	grafo.agregar_arco(4,6,0);
+	grafo.agregar_arco(4,11,0);
+	grafo.agregar_arco(5,11,0);
+	grafo.agregar_arco(5,10,0);
+	grafo.agregar_arco(6,11,0);
+	grafo.agregar_arco(6,8,0);
+    grafo.agregar_arco(7,8,0);
+	grafo.agregar_arco(7,12,0);
+	grafo.agregar_arco(8,12,0);
+	grafo.agregar_arco(8,9,0);
+	grafo.agregar_arco(8,11,0);
+	grafo.agregar_arco(9,11,0);
+	grafo.agregar_arco(9,10,0);
+	grafo.agregar_arco(9,12,0);
+	grafo.agregar_arco(10,11,0);
+
+    CoberturaBomberos(grafo);
+    return 0;
+}
+
+bool OrdenPorSeg(const pair<int,int> &a,const pair<int,int> &b){
+    return (a.second > b.second);
+}
+
+void OrdenarDec(vector<pair<int,int>> &lista){
+    sort(lista.begin(), lista.end(), OrdenPorSeg);
+}
+
+void ActualizarAdyacentes(vector<pair<int,int>> &barrios,Grafo<int> grafo,list<int> vertices){ //O(v^2)
+            for(auto it=vertices.begin();it!=vertices.end();it++){
+                barrios.push_back(make_pair(*it,grafo.cantidad_adyacentes(*it))); //O(v)
+            }
+            OrdenarDec(barrios);
+}
+
+
+void CoberturaBomberos(Grafo<int> grafo){
+
+    vector<int> solucion;
+    list<int> vertices;
+    vector<pair<int,int>> barrios;
+    grafo.devolver_vertices(vertices);
+
+    ActualizarAdyacentes(barrios,grafo,vertices);
+
+    auto it=barrios.begin();
+    while(!barrios.empty()){    //Agrega el vertice con mas cantidad de vecinos sin cubrir a la solución en cada iteracion.
+
+        solucion.push_back(it->first);
+        vertices.remove(it->first);
+        list<pair<int,int>> adyacentes;
+        grafo.devolver_adyacentes(it->first,adyacentes);
+        grafo.eliminar_vertice(it->first);
+        for(auto itAdy=adyacentes.begin();itAdy!=adyacentes.end();itAdy++){
+            vertices.remove(itAdy->first);
+            grafo.eliminar_vertice(itAdy->first);
+        }
+        barrios.clear();
+        ActualizarAdyacentes(barrios,grafo,vertices);
+
+    };
+
+    cout<<"Solucion posible: ";
+    for(auto it=solucion.begin();it!=solucion.end();it++){
+        cout<< *it<<"  ";
+    }
+}
+
+
